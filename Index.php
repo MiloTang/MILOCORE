226211d202a2390757a760cc16dd355c7f1ec3e9
miloit <miloit@qq.com>
2016-12-26 18:04:42 +0800

    20161226

--- Index.php
+++ Index.php
@@ -4,8 +4,17 @@
  * User: Milo
  * Date: 2016/11/12
  * Time: 20:26
+ * 数据库处理加强
+ * 内存数据库的处理
+ * Ｎｏｓｑｌ数据库的处理
+ * 日志的处理
+ * Ｃｏｍｐｏｓｅｒ加入第三方库的处理
+ * 二维码生成
+ * ＥＸＣＥＬ处理
+ * 邮件发送
+ *
  */
-define('APP_NAME','App');
+define('APP_NAME','WeiShop');
 define('DEBUG',true);
 //define('URL_SECRET',true);
 require ('./Core/MiloCore.class.php');
